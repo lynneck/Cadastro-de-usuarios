47efa9b9ef2bf534c5aafd85fc7e6dfb51f98ea2
unknown <seu@email.comlynneckmartins@hotmail.com>
2021-06-26 13:49:58 -0300

    initial

---
+++ src/main/java/com/prova/questao1/FormularioController.java
@@ -0,0 +1,11 @@
+package com.prova.questao1;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FormularioController {
+    public  String usuario(){
+        return "index" ;
+    }
+
+}
---
+++ src/main/java/com/prova/questao1/Questao1Application.java
@@ -0,0 +1,13 @@
+package com.prova.questao1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Questao1Application.class, args);
+	}
+
+}
